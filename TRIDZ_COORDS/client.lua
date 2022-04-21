cec8f573a1d0ecd654f8bd2301efe286da86e819
Tridz5m <104125973+Tridz5m@users.noreply.github.com>
2022-04-21 16:28:09 +0200

    Update client.lua

--- TRIDZ_COORDS/client.lua
+++ TRIDZ_COORDS/client.lua
@@ -1,16 +1,16 @@
-local ped = GetPlayerPed(-1)
-local coords = GetEntityCoords
+local ped = GetPlayerPed(-1) -- This will get your Player Ped.
+local coords = GetEntityCoords -- This will get your Player Ped's coordinates.
 
-RegisterCommand("coords", function(source, args, rawCommand)
+RegisterCommand("coords", function(source, args, rawCommand) -- Makes the command
    
-    print(GetEntityCoords(GetPlayerPed(-1)))
+    print(table.unpack(GetEntityCoords(GetPlayerPed(-1)))) -- This will print your coordinates in your f8 console.
 
-    msg('Your coordinates are pasted in your F8 console.')
+    msg('Your coordinates are pasted in your F8 console.') -- Message when done
 
 end)
 
 
 
 function msg(text)
-    TriggerEvent("chatMessage", "[TRIDZ_COORDS]", {255, 0, 0}, text)
+    TriggerEvent("chatMessage", "[TRIDZ_COORDS]", {255, 0, 0}, text) -- message function
 end
